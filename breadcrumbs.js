78a19805c2649b423a15db75edcafde5a83d8721
Mathias Hammer <TiccoNz@users.noreply.github.com>
2017-11-18 16:48:23 +0100

    update

--- breadcrumbs.js
+++ breadcrumbs.js
@@ -1,22 +1,22 @@
 //const url = "http://www.webmatematik.dk/lektioner/matematik-b/trigonometri/cosinusrelationerne"
 
-
-const makeBreadcrumb = () => {
-    let breadcrumb = ""
-
-    for (i = 3; i < splitUrl.length; 
 const url = "http://sciencemadeeasy.dk/IT/Programming/JavaScript"
 
 const splitUrl = url.split('/')
 
 splitUrl
 
-console.log(splitUrl[3])i++) {
+const makeBreadcrumb = () => {
+    let breadcrumb = ""
+
+    for (i = 3; i < splitUrl.length; i++) {
         breadcrumb = breadcrumb + splitUrl[i] + ' ' + '/' + ' '
     }
 
     return breadcrumb
 }
+
+console.log(splitUrl[3])
 
 splitUrl <= 3 ? false : makeBreadcrumb() 
 
